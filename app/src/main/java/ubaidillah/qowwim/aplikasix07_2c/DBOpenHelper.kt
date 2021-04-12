697122952e84c3ee35e05780ecd42331479aa182
ubaidillah1 <ubayyubaid@gmail.com>
2021-04-12 16:25:15 +0700

    first commit

--- app/src/main/java/ubaidillah/qowwim/aplikasix07_2c/DBOpenHelper.kt
+++ app/src/main/java/ubaidillah/qowwim/aplikasix07_2c/DBOpenHelper.kt
@@ -1,4 +1,22 @@
 package ubaidillah.qowwim.aplikasix07_2c
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 
-class DBOpenHelper {
+class DBOpenHelper(context: Context): SQLiteOpenHelper(context,DB_Name,null,DB_Ver) {
+    companion object{
+        val DB_Name = "prak7"
+        val DB_Ver = 1
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val tData = "create table listkalimat(idkal integer primary key autoincrement,nama text not null)"
+        val insSample = "insert into listkalimat(nama) values('Manajemen Informatika')"
+        db?.execSQL(tData)
+        db?.execSQL(insSample)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
--- app/src/main/java/ubaidillah/qowwim/aplikasix07_2c/MainActivity.kt
+++ app/src/main/java/ubaidillah/qowwim/aplikasix07_2c/MainActivity.kt
@@ -1,19 +1,27 @@
 package ubaidillah.qowwim.aplikasix07_2c
 
+import android.content.ContentValues
+import android.content.DialogInterface
 import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.integration.android.IntentIntegrator
-import com.google.zxing.integration.android.IntentResult
 import com.journeyapps.barcodescanner.BarcodeEncoder
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-
+    lateinit var db: SQLiteDatabase
+    lateinit var adapter: ListAdapter
+    lateinit var v : View
+    lateinit var builder: AlertDialog.Builder
+    var idList : String = ""
     override fun onClick(v: View?) {
         when(v?.id){
             R.id.btnScanQr->{
@@ -23,22 +31,59 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val barCodeEncoder = BarcodeEncoder()
                 val bitmap = barCodeEncoder.encodeBitmap(edQrCode.text.toString(),
                 BarcodeFormat.QR_CODE,400,400)
-                QrCodeView.setImageBitmap(bitmap)
+//                QrCodeView.setImageBitmap(bitmap)
+                if(bitmap!=null){
+                    if( edQrCode!= null){
+                        QrCodeView.setImageBitmap(bitmap)
+                    }else{
+                        Toast.makeText(this,"Dibatalkan",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            R.id.btnHapus->{
+                builder.setTitle("Konfirmasi").setMessage("Yakin akan menghapus data ini?")
+                        .setIcon(android.R.drawable.ic_dialog_info)
+                        .setPositiveButton("Ya",btnDeleteList)
+                        .setNegativeButton("Tidak",null)
+                builder.show()
+            }
+            R.id.btnEdit->{
+                builder.setTitle("Konfirmasi").setMessage("Apakah data nya sudah benar ?")
+                        .setIcon(android.R.drawable.ic_dialog_info)
+                        .setPositiveButton("Ya",btnUpdateDialog)
+                        .setNegativeButton("Tidak",null)
+                builder.show()
             }
         }
     }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val intentResult : IntentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data)
+        val intentResult  = IntentIntegrator.parseActivityResult(requestCode,resultCode,data)
         if(intentResult!=null){
             if(intentResult.contents != null){
+
                 edQrCode.setText(intentResult.contents)
+                var kata = edQrCode.text
+                var cv : ContentValues = ContentValues()
+                cv.put("nama",kata.toString())
+                db.insert("listkalimat",null,cv)
             }else{
                 Toast.makeText(this,"Dibatalkan",Toast.LENGTH_SHORT).show()
             }
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
+    val itemClick = AdapterView.OnItemClickListener{ parent, view, position, id ->
+        val c: Cursor = parent.adapter.getItem(position) as Cursor
+        idList = c.getString(c.getColumnIndex("_id"))
+        edQrCode.setText(c.getString(c.getColumnIndex("nama")))
+    }
+//    fun insertDataList(Kalimat:String){
+//        var cv : ContentValues = ContentValues()
+//        cv.put("nama",Kalimat)
+//        db.insert("listkalimat",null,cv)
+//        showDataList()
+//        Toast.makeText(this,"Data Berhasil Ditambahkan",Toast.LENGTH_SHORT).show()
+//    }
     lateinit var intentIntegrator: IntentIntegrator
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,5 +92,43 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         intentIntegrator = IntentIntegrator(this)
         btnGenereateQr.setOnClickListener (this)
         btnScanQr.setOnClickListener (this)
+
+        btnHapus.setOnClickListener(this)
+        btnEdit.setOnClickListener(this)
+        builder = AlertDialog.Builder(this)
+        lsViewData.setOnItemClickListener(itemClick)
+        db = DBOpenHelper(this).writableDatabase
+
+    }
+    fun showDataList() {
+        val cursor : Cursor =db.query("listkalimat", arrayOf("nama","idkal as _id"),
+                null,null,null,null,"_id asc")
+        adapter = SimpleCursorAdapter(this,R.layout.data_list,cursor,
+                arrayOf("_id","nama"), intArrayOf(R.id.txId, R.id.txKalimat), CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER)
+        lsViewData.adapter = adapter
+
+    }
+    fun deleteDataList(idkalimat: String){
+        db.delete("listkalimat","idkal = $idkalimat", null)
+        showDataList()
+    }
+    val btnDeleteList = DialogInterface.OnClickListener{ dialog, which ->
+        deleteDataList(idList)
+        edQrCode.setText("")
+    }
+    fun updateDataList(idkalimat: String ,idKal: String){
+        var cv : ContentValues = ContentValues()
+        cv.put("nama",idkalimat)
+        db.update("listkalimat",cv,"idkal = $idKal",null)
+        showDataList()
+    }
+    val btnUpdateDialog = DialogInterface.OnClickListener{ dialog, which ->
+        updateDataList(edQrCode.text.toString(),idList)
+        edQrCode.setText("")
+    }
+    override fun onStart() {
+        super.onStart()
+        showDataList()
+//        insertDataList(edQrCode.toString())
     }
 }
